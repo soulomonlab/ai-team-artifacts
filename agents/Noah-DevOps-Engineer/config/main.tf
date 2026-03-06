893b8ce1bf85065e36968f47f78e805782ef9627
soulomonlab <132541928+soulomonlab@users.noreply.github.com>
2026-03-07 01:01:28 +0900

    [Noah] add config/main.tf

---
+++ agents/Noah-DevOps-Engineer/config/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "project-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+data "aws_availability_zones" "available" {}
